e5a692e68cdf3f9874b0b1406f414bc40a532e9a
thechimes <caleececconi@gmail.com>
2022-07-06 15:17:12 -0500

    Making a 2nd draft

---
+++ mcad/troubleshooting/SU22_amyJoF_P1/draft2/calee_edits-images.js
@@ -0,0 +1,39 @@
+let imgs = [];
+
+function preload() {
+    myFont = loadFont('fonts/Coronette.otf');
+    for (var i = 0; i < 8; i++) {
+        imgs[i] = loadImage("img/img_" + i + ".png");
+    }
+}
+
+function setup() {
+    createCanvas(612, 792);//this is the size of a letter-sized page in 72 dpi
+    background(255);
+    imageMode(CENTER);
+    canvas.parent('container');
+}
+
+function draw() {
+  //I removed the text from the p5 file so that it's just the image randomizer to make it easier to deal with
+}
+
+function mouseClicked() {
+    var index = int(random(8));
+    image(imgs[index], width/2, height/2);//Centering the image on the canvas—this is responsive to whatever your canvas size is
+}
+
+
+function keyTyped() {
+    //clear screen
+    if (key === 'c') {
+        clear();
+        background(255);
+    }
+    //screenshot 
+    if (key == 's') {
+        save('protestposter.png');
+    }
+}
+
+// ref:multi click images - https://editor.p5js.org/NicolasTilly/sketches/dZUpAB3rd https://discourse.processing.org/t/display-images-randomly-by-clicking/17380
