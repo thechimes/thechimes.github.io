885668c79eb81a51ed63162bc2e9177e27c5e7eb
thechimes <caleececconi@gmail.com>
2022-07-08 16:05:47 -0500

    putting canvas in the middle of the page

--- continuing_ed/superhi/experimental_type/p3_0101/scripts/sketch.js
+++ continuing_ed/superhi/experimental_type/p3_0101/scripts/sketch.js
@@ -1,8 +1,17 @@
-
-function setup () {
-
+function preload(){
+    spaceGrotesk = loadFont('assets/spacegrotesk-medium.otf');
 }
 
-function draw () {
-    ellipse(100, 100, 100,100); 
+function setup(){
+    createCanvas(600,600);
+    textFont(spaceGrotesk);
+}
+
+function draw(){
+    fill("#ef5236");
+    textFont(spaceGrotesk);
+    textSize(400);
+    textAlign(CENTER);
+    text("01", width/2, height/2); 
+
 }
