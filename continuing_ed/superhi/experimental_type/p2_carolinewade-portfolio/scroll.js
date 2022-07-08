079e0b5866e38c3ff5bf479d7db9884dafd5dfe8
thechimes <caleececconi@gmail.com>
2022-07-08 14:31:37 -0500

    fixing the js fiel

--- continuing_ed/superhi/experimental_type/p2_carolinewade-portfolio/scroll.js
+++ continuing_ed/superhi/experimental_type/p2_carolinewade-portfolio/scroll.js
@@ -1,11 +1,15 @@
 // choose the h1 style
 const h1 = document.querySelector("h1")
 
-// tell it to change width/weight on scroll
+// tell it to change width and weight on scroll
 
-window.addEventListener("scroll", function{
+window.addEventListener("scroll", function (){
     const pixels = window.scrollY
 
-    h1.style.fontSize = pixels + "px"
+    const wght = 100 + pixels * 0.4
+    const wdth = 100 + pixels * 0.1
+    
+    h1.style.fontVariationSettings = `"wght" ${wght}, "wdth" ${wdth}`
     }
 )
+
