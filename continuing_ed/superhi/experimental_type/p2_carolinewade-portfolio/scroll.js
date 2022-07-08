c746dad26e884d0550d6e91ff277359d32de5dd4
thechimes <caleececconi@gmail.com>
2022-07-08 14:21:15 -0500

    Updating the scrolling changes and the style.css files

--- continuing_ed/superhi/experimental_type/p2_carolinewade-portfolio/scroll.js
+++ continuing_ed/superhi/experimental_type/p2_carolinewade-portfolio/scroll.js
@@ -1,14 +1,3 @@
-// pick the H1 tag
-const h1 = document.querySelector("h1");
+// choose the h1 style
 
-// and when I scroll calculate how big the font size should be 
-window.addEventListener("scroll", function(){
-    const pixels = window.pageYOffset
-
-    const fontWeight = pixels * 0.4 + 100
-
-    const fontWidth = pixels * 0.1 + 100
-
-    h1.style.fontVariationSettings = '"wght" ${fontWeight}, "wdth" ${fontWidth}'
-    }
-)
+// tell it to change size/weight on scroll
