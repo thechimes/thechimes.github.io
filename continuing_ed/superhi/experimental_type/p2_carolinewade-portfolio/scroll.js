bfd75a54250f712090bc4cd08e4bd5d05fbab3af
thechimes <caleececconi@gmail.com>
2022-07-08 14:28:14 -0500

    trying a different thing

--- continuing_ed/superhi/experimental_type/p2_carolinewade-portfolio/scroll.js
+++ continuing_ed/superhi/experimental_type/p2_carolinewade-portfolio/scroll.js
@@ -1,11 +1,11 @@
 // choose the h1 style
-const h1 = document.querySelector("h1");
+const h1 = document.querySelector("h1")
 
 // tell it to change width/weight on scroll
 
 window.addEventListener("scroll", function{
-    const pixels = window.pageYOffset;
+    const pixels = window.scrollY
 
-    h1.style.fontSize = pixels + "px";
+    h1.style.fontSize = pixels + "px"
     }
 )
