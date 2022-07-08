22bcd3e497fe6fe694123da7b19585b77e1474ca
thechimes <caleececconi@gmail.com>
2022-07-08 14:26:39 -0500

    updating scroll.js

--- continuing_ed/superhi/experimental_type/p2_carolinewade-portfolio/scroll.js
+++ continuing_ed/superhi/experimental_type/p2_carolinewade-portfolio/scroll.js
@@ -1,3 +1,11 @@
 // choose the h1 style
+const h1 = document.querySelector("h1");
 
-// tell it to change size/weight on scroll
+// tell it to change width/weight on scroll
+
+window.addEventListener("scroll", function{
+    const pixels = window.pageYOffset;
+
+    h1.style.fontSize = pixels + "px";
+    }
+)
