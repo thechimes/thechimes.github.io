ca7153ed84490edc64a4cab60cce17cfc27685f9
thechimes <caleececconi@gmail.com>
2022-07-11 15:04:02 -0500

    organizing files to do homework

---
+++ continuing_ed/superhi/experimental_type/p3_0101/original/scripts/sketch.js
@@ -0,0 +1,29 @@
+let spaceGrotesk;
+let graphic;
+
+function preload(){
+    spaceGrotesk = loadFont('assets/spacegrotesk-medium.otf');
+}
+
+function setup(){
+    createCanvas(500,500);
+    textFont(spaceGrotesk);
+    //creating a graphic that you can stamp onto other parts of the page
+    graphic = createGraphics(500,500);
+    graphic.
+    fill("#ef5236");
+    graphic.textFont(spaceGrotesk);
+    graphic.textSize(350);
+    graphic.textAlign(CENTER);
+    graphic.text("01", width/2, height/2); 
+}
+
+function draw(){
+    background("#ecf072");
+    //copy the graphic to the page first 4 numbers are what it looks like in its original form and the last ones are what it should look like when you stamp it
+
+    let val = sin(frameCount * 0.5) * 250; 
+
+    copy(graphic, 0,0,500,500,0,0, 250+ val,500);
+    copy(graphic,0,0,500,500,250 + val,0,250 - val, 500);
+}
