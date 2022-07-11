3b0960e7f915f38669d604de277b46b991b57eb5
thechimes <caleececconi@gmail.com>
2022-07-11 15:01:22 -0500

    Final

--- continuing_ed/superhi/experimental_type/p4_aaa/scripts/sketch.js
+++ continuing_ed/superhi/experimental_type/p4_aaa/scripts/sketch.js
@@ -7,7 +7,6 @@ function preload(){
 
 function setup(){
     createCanvas(600,600);
-
     //create graphic
     graphic = createGraphics(600,600)
     // define the graphic 
@@ -20,6 +19,35 @@ function setup(){
 }
 
 function draw(){
-    backgroundColor("#ef5236");
+    background("#ef5236");
+    //create a tile 
+    const tileSize = 100;
 
+    //first time x is 0, and then we add one for every time we run this until x = 5 for the X
+    //horizontal direction
+    for(let x = 0; x < 12; x = x + 1){
+
+        //add the vertical direction
+        for(let y = 0; y < 12; y = y + 1){
+        
+        //add distortion
+        const distortion = sin(0.05 * frameCount + x * 0.5 + y * 0.5) * 50;
+        
+        // source info variables—if you change these, it changes what we are sampling.
+        const sx = x * tileSize + distortion;
+        const sy = y * tileSize;
+        const sw = tileSize;
+        const sh = tileSize;
+
+        //destination info variables
+        const dx = x * tileSize;
+        const dy = y * tileSize;
+        const dw = tileSize;
+        const dh = tileSize;
+
+        // Copy is really slow! this will slow everything down. Copy clones things pixel by pixel. Instead we are going to use image to save our created graphic as an image. We can use this with the createGraphics as well as an image we are loading. Copy and image have opposite areas for the source and the destination, just FYI.
+        image(graphic, dx, dy, dw, dh, sx, sy, sw, sh);   
+
+        } 
+    }
 }
