e0e2172385ade3cb59ebe3bc14522bd9b6cbfe66
thechimes <caleececconi@gmail.com>
2022-07-18 10:45:55 -0500

    Final edtis

--- continuing_ed/superhi/experimental_type/p5_good-morning/scripts/sketch.js
+++ continuing_ed/superhi/experimental_type/p5_good-morning/scripts/sketch.js
@@ -11,28 +11,33 @@ function setup(){
     graphic = createGraphics(1200,600);
     graphic.textFont(spaceGrotesk);
     graphic.textAlign(CENTER, CENTER);
-    graphic.textSize(100);
-    graphic.textLeading(100);
+    graphic.textSize(200);
+    graphic.textLeading(150);
     graphic.fill("#F96AA2");
     graphic.text("Good\nMorning", 600,300);
 
-   // noLoop();
+   noLoop();
 }
 
 function draw(){
     background("#F0F0EE");
-
     //split up into 10px sections
 
-    const tileSize = 100;
+    const tileSize = 10;
 
-    for(x = 0; x < 12; x = x + 1){
+    for(x = 0; x < 120; x = x + 1){
         
-        for(y = 0; y < 6; y = y + 1){
+        for(y = 0; y < 60; y = y + 1){
+
+            //set distortion
+            // tell it how fast to move
+            const wave = 0.05; 
+            const distortionX = sin(frameCount * wave + x * 0.5 + y * 0.1) * 10;
+            const distortionY = sin(frameCount * wave + x * 0.5 + y * 1) * 5;
             
             //set source dimensions
-            const sx = x * tileSize;
-            const sy = y * tileSize;
+            const sx = x * tileSize + distortionX;
+            const sy = y * tileSize + distortionY;
             const sw = tileSize;
             const sh = tileSize; 
 
